22c7db1af1618eec35544ef9cae72304dc874729
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-23 21:24:20 -0400

    finish the styles

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useStyles from './styles';
 
 const Form = () => {
+    const classes = useStyle();
+
     return (
         <h1>FORM</h1>
     )
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useStyles from './styles';
 
 const Post = () => {
+    const classes = useStyle();
+
     return (
         <h1>Post</h1>
     )
